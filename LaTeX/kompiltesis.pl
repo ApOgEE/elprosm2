5d575774a18c65ac9458580685a2601e3fce3eb1
M. Fauzilkamil Zainuddin <jerungkun@gmail.com>
2011-01-18 09:29:14 +0800

    tambah skrip perl pengkompil. setakat ni takde faedah lagi guna skrip ni.

---
+++ LaTeX/kompiltesis.pl
@@ -0,0 +1,67 @@
+#!/usr/bin/perl -w
+#
+# @ Pengarang: M. Fauzilkamil Zainuddin
+# @ Tajuk: Skrip Perl untuk mengkompil psm2-awustel.tex
+# @ Copyright (C) 2011, M. Fauzilkamil Zainuddin
+#
+# @ Keterangan:
+#	Oleh kerana saya suka mengarut-ngarut dalam kerja yang saya lakukan,
+#	saya membuat skrip ini untuk memudahkan kerja kompilasi saya.
+#
+# @ Cara Penggunaan:
+#	Saya membayangkan skrip ini untuk digunakan seperti ini:
+#	
+#	Untuk mengkompil PDF
+#		$ ./kompiltesis.pl kompil
+#
+#	Untuk membuang fail sampah yang dihasilkan setelah kompilasi
+#	tak berjaya.
+#		$ ./kompiltesis.pl cuci
+#
+# @ Nota Kaki Bangku:
+# 	kod ini masih lagi ditahap hampas dan tiada faedah menggunakannya
+#	Jika anda mahu menggunakannya juga, risiko tanggung sendiri.
+#
+
+sub kepala {
+	# bahagian ni tak buat apa kerja berfaedah pun. Hanya untuk 
+	# memaparkan nama dan versi skrip ni je.
+	print "[@] Skrip Pengkompil psm2-awustel.tex versi 0.0.0\n";
+	print "[@] Copyright (C) 2011, M. Fauzilkamil Zainuddin\n";
+	print "    ---------------------------------------------\n\n"
+}
+
+sub kompil {
+	kepala();
+	print "Mari kita mengkompil tesis!\n\n";	
+}
+
+sub cuci {
+	kepala();
+	print "Mari kita membuang sampah...\n\n";
+}
+
+sub cara {
+	print "/!\\ Setakat ni, ada dua cara je nak guna skrip ni... jangan buat pasal!\n\n";
+	print " Cara mengkompil PDF:\n";
+	print "    \$ ./kompiltesis.pl kompil\n\n";
+	print " Cara membuang fail sampah setelah pengkompilan terfakap:\n";
+	print "    \$ ./kompiltesis.pl cuci\n\n";
+}
+
+use Switch;
+
+my $suis = "";
+
+if ( $#ARGV != 0) {
+	cara();
+	exit;
+}
+$suis = shift(@ARGV);
+
+switch ($suis) {
+	case 'kompil'	{ kompil(); }
+	case 'cuci'	{ cuci(); }
+	else 		{ cara(); }
+}
+
