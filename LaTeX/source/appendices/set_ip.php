a641849be0b588d32714b5aeb3199f80bdb54e4a
M. Fauzilkamil Zainuddin <jerungkun@gmail.com>
2011-11-21 06:24:37 +0800

    update senarai kandungan

---
+++ LaTeX/source/appendices/set_ip.php
@@ -0,0 +1,17 @@
+<?php
+
+function set_device_ip() {
+	echo "setting device IP";
+}
+
+?>
+<html>
+<head>
+<title>Set Device IP</title>
+</head>
+<body>
+<div>
+Set Device IP
+</div>
+</body>
+</html>
