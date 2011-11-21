fc79db40efc73af03f709aece4491e5b38133f2d
M.Fauzilkamil Zainuddin <jerungkun@gmail.com>
2011-11-21 12:36:58 +0800

    tambah kod sumber elwiface - embedded linux web interface

---
+++ src/elwiface/index.php
@@ -0,0 +1,20 @@
+<?php
+
+// main page
+// check user login
+
+?>
+<html>
+<head>
+<title>
+Realtime Raingauge
+</title>
+<link type="text/css" rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div id="header">
+<h1>Realtime Raingauge</h1>
+</div>
+</body>
+</html>
+
